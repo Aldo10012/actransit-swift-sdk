30497924fcdbf989751d64128e60ff4ea7017b78
Alberto <aldo10012@gmail.com>
2026-05-18 15:59:51 -0700

    add dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "ACTransitSwift",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +19,19 @@ let package = Package(
             targets: ["ACTransitSwift"]
         ),
     ],
+    dependencies: [
+        // 1. Declare the remote package dependency
+        .package(url: "https://github.com/Aldo10012/EZNetworking", from: "5.6.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ACTransitSwift"
+            name: "ACTransitSwift",
+            dependencies: [
+                // 2. Link the EZNetworking product to your target
+                .product(name: "EZNetworking", package: "EZNetworking")
+            ]
         ),
         .testTarget(
             name: "ACTransitSwiftTests",
