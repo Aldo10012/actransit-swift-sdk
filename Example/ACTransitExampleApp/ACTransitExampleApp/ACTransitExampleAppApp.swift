090503354de6db2784926d6d9ef7f198b26c30c3
Alberto <aldo10012@gmail.com>
2026-05-29 19:06:31 -0700

    add empty example xcode project - will use to show examples of how to use API

---
+++ Example/ACTransitExampleApp/ACTransitExampleApp/ACTransitExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACTransitExampleAppApp.swift
+//  ACTransitExampleApp
+//
+//  Created by Alberto Dominguez on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ACTransitExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
